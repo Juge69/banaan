5472b3373a3b90a4edc53912a8cb8c2b6db6b380
Julian <133595442+Juge69@users.noreply.github.com>
2025-05-24 17:03:27 +0200

    current work in progress

---
+++ python/game.py
@@ -0,0 +1,25 @@
+print("welcome to the forest of illusions")
+print("you are lost in the forest and need to find your way out")
+print("you can go north, south, east or west")
+
+directionChoise = input("which direction do you want to go? ")
+if directionChoise == "north":
+    print("you find a river and decide to follow it")
+    print("after a while you find a bridge and cross it")
+    print("Suprise! you find a village on the other side")
+    print("you can stock up on supplies and rest here")
+elif directionChoise == "south":
+    print("you find a cave and decide to explore it")
+    print("inside the cave you find a treasure chest")
+    print("you open the chest and find gold coins and jewels")
+    print("you can take some treasure and leave the cave")
+elif directionChoise == "east":
+    print("you find a path leading to a mountain")
+    print("you climb the mountain and reach the top")
+    print("from the top you can see the whole forest")
+    print("you can take a moment to enjoy the view")
+elif directionChoise == "west":
+    print("you find a path leading to a waterfall")
+    print("you follow the path and reach the waterfall")
+    print("you can take a swim in the pool below the waterfall")
+    print("after swimming you feel refreshed and ready to continue your journey")
